66ed9e4626d613959dde755b2e01465be9697ecb
Mike Stevens <25899552-MichaelSteven37@users.noreply.replit.com>
2025-05-13 01:54:10 +0000

    Fix CRA fallback imports by disabling core polyfills

--- client/craco.config.js
+++ client/craco.config.js
@@ -4,11 +4,11 @@ module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.resolve.fallback = {
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        os: require.resolve("os-browserify/browser"),
-        stream: require.resolve("stream-browserify"),
-        zlib: require.resolve("browserify-zlib"),
+        http: false,
+        https: false,
+        os: false,
+        stream: false,
+        zlib: false,
       };
       return webpackConfig;
     },
