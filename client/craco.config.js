da03d0cfb5831d8ad8c1dab9df5c63614f5148ab
Mike Stevens <25899552-MichaelSteven37@users.noreply.replit.com>
2025-05-13 01:48:24 +0000

    Fix malformed craco.config.js

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,19 +1,22 @@
 const webpack = require("webpack");
 
 module.exports = {
-  resolve: {
-    fallback: {
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      os: require.resolve("os-browserify/browser"),
-      stream: require.resolve("stream-browserify"),
-      zlib: require.resolve("browserify-zlib"),
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        stream: require.resolve("stream-browserify"),
+        zlib: require.resolve("browserify-zlib"),
+      };
+      return webpackConfig;
     },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+        Buffer: ["buffer", "Buffer"],
+      }),
+    ],
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-      process: "process/browser",
-      Buffer: ["buffer", "Buffer"],
-    }),
-  ],
 };
